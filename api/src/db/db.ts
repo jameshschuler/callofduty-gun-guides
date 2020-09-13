091c73f62bf58d58a8da1b401b18773659b1596c
unknown <jameshschuler1@gmail.com>
2020-09-13 16:14:27 -0400

    updated db config and added start command

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -12,7 +12,8 @@ const config = {
             user: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             port: process.env.POSTGRES_PORT,
-            host: process.env.POSTGRES_HOST
+            host: process.env.POSTGRES_HOST,
+            ssl: true
         } as PgConnectionConfig
     }
 } as any[ string ];
