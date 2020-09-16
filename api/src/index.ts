41a9bc1d5ecb90d4846d2fd4be7fcdc81010e954
jameshschuler1 <jameshschuler1@gmail.com>
2020-09-15 23:07:29 -0400

    refactoring

--- api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import logger from './lib/logger';
-
-const port = process.env.PORT || 5000;
-app.listen( port, () => {
-  logger.info( `Listening at http://localhost:${port}` );
-} );
