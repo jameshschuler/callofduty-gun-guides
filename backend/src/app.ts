9e9a4e405d83ecd7927a9cf238c9d2620f8e41aa
James Schuler <jameshschuler1@gmail.com>
2020-12-16 20:13:03 -0500

    added rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import Knex from 'knex';
 import morgan from 'morgan';
@@ -21,6 +22,13 @@ app.use( compression() );
 app.use( helmet() );
 app.use( express.json( { type: 'application/json' } ) );
 app.use( cors() );
+const limiter = rateLimit( {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50
+} );
+
+//  apply to all requests
+app.use( limiter );
 
 app.get( '/', ( req: express.Request, res: express.Response ) => {
     res.json( {
